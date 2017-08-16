4be893fdaf53f882155a5fdd4c41bc2024f5f059
Davide Perozzi <davide@dorfjungs.com>
2017-08-16 02:10:36 +0200

    UPDATE made swiper slide method arguments optional

--- externs.js
+++ externs.js
@@ -1600,23 +1600,23 @@ Swiper.prototype.bullets;
 
 /**
  * @public
- * @param {boolean} runCallbacks
- * @param {number} speed
+ * @param {boolean=} runCallbacks
+ * @param {number=} speed
  */
 Swiper.prototype.slideNext = function(runCallbacks, speed){};
 
 /**
  * @public
- * @param {boolean} runCallbacks
- * @param {number} speed
+ * @param {boolean=} runCallbacks
+ * @param {number=} speed
  */
 Swiper.prototype.slidePrev = function(runCallbacks, speed){};
 
 /**
  * @public
  * @param {number} index
- * @param {number} speed
- * @param {boolean} runCallbacks
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
  */
 Swiper.prototype.slideTo = function(index, speed, runCallbacks){};
 
