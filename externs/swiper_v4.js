977f8659fba3c8a427dcfd8bf620365b9e97abe9
Joshua Keck <joshua.keck@dorfjungs.com>
2017-12-21 22:10:12 +0100

    Added: Swiper v4 externs

---
+++ externs/swiper_v4.js
@@ -0,0 +1,128 @@
+/**
+ * @externs
+ */
+
+/**
+ * @constructor
+ * @param {Element|string} target
+ * @param {Object} config
+ */
+var Swiper = function(target, config){};
+
+/**
+ * @public
+ * @type {boolean}
+ */
+Swiper.prototype.animating;
+
+/**
+ * @public
+ * @type {number}
+ */
+Swiper.prototype.realIndex;
+
+/**
+ * @public
+ * @type {number}
+ */
+Swiper.prototype.activeIndex;
+
+/**
+ * @public
+ * @type {Array<Element>}
+ */
+Swiper.prototype.slides;
+
+/**
+ * @public
+ * @type {Element}
+ */
+Swiper.prototype.pagination.el;
+
+/**
+ * @public
+ * @type {Array<Element>}
+ */
+Swiper.prototype.pagination.bullets;
+
+/**
+ * @public
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
+ */
+Swiper.prototype.slideNext = function(speed, runCallbacks){};
+
+/**
+ * @public
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
+ */
+Swiper.prototype.slidePrev = function(speed, runCallbacks){};
+
+/**
+ * @public
+ * @param {number} index
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
+ */
+Swiper.prototype.slideTo = function(index, speed, runCallbacks){};
+
+/**
+ * @public
+ * @param {boolean} deleteInstance
+ * @param {boolean} cleanupStyles
+ */
+Swiper.prototype.destroy = function(deleteInstance, cleanupStyles){};
+
+/**
+ * @public
+ */
+Swiper.prototype.autoplay.start = function(){};
+
+/**
+ * @public
+ */
+Swiper.prototype.autoplay.stop = function(){};
+
+/**
+ * @public
+ */
+Swiper.prototype.update = function(){};
+
+/**
+ * @public
+ */
+Swiper.prototype.updateSize = function(){}
+
+/**
+ * @public
+ */
+Swiper.prototype.updateSlides = function(){}
+
+/**
+ * @public
+ */
+Swiper.prototype.updateProgress = function(){}
+
+/**
+ * @public
+ */
+Swiper.prototype.updateSlidesClasses = function(){}
+
+/**
+ * @public
+ * @param {Element|string|Array<Element>} slide
+ */
+Swiper.prototype.prependSlide = function(slide){};
+
+/**
+ * @public
+ * @param {Element|string|Array<Element>} slide
+ */
+Swiper.prototype.appendSlide = function(slide){};
+
+/**
+ * @public
+ * @param {Array<number>|number} slideIndex
+ */
+Swiper.prototype.removeSlide = function(slideIndex){};
