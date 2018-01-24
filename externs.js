47ad67829650f8a070b0190fdaf407a690d18eb4
Joshua Keck <joshua@dorfjungs.com>
2018-01-24 10:32:47 +0100

    Added: Packery.js

--- externs.js
+++ externs.js
@@ -1653,6 +1653,27 @@ Masonry.prototype.addItems = function(elems){};
 
 /**
  * @constructor
+ * @param {Element} wrapper
+ * @param {Object} config
+ */
+var Packery = function(wrapper, config){};
+
+/**
+ * @public
+ */
+Packery.prototype.layout = function(){};
+
+/**
+ * @public
+ * @param {Array<Element>|Element} elems
+ */
+Packery.prototype.addItems = function(elems){};
+/**
+ * @externs
+ */
+
+/**
+ * @constructor
  * @param {Element|string} target
  * @param {Object} config
  */
@@ -1761,3 +1782,143 @@ Swiper.prototype.removeSlide = function(slideIndex){};
  * @public
  */
 Swiper.prototype.updateAutoHeight = function(){};
+/**
+ * @externs
+ */
+
+/**
+ * @constructor
+ * @param {Element|string} target
+ * @param {Object} config
+ */
+var Swiper = function(target, config){};
+
+/**
+ * @public
+ * @type {boolean}
+ */
+Swiper.prototype.animating;
+
+/**
+ * @public
+ * @type {number}
+ */
+Swiper.prototype.realIndex;
+
+/**
+ * @public
+ * @type {number}
+ */
+Swiper.prototype.activeIndex;
+
+/**
+ * @public
+ * @type {Array<Element>}
+ */
+Swiper.prototype.slides;
+
+/**
+ * @public
+ * @type {Object}
+ */
+Swiper.prototype.pagination;
+
+/**
+ * @public
+ * @type {Element}
+ */
+Swiper.prototype.pagination.el;
+
+/**
+ * @public
+ * @type {Array<Element>}
+ */
+Swiper.prototype.pagination.bullets;
+
+/**
+ * @public
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
+ */
+Swiper.prototype.slideNext = function(speed, runCallbacks){};
+
+/**
+ * @public
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
+ */
+Swiper.prototype.slidePrev = function(speed, runCallbacks){};
+
+/**
+ * @public
+ * @param {number} index
+ * @param {number=} speed
+ * @param {boolean=} runCallbacks
+ */
+Swiper.prototype.slideTo = function(index, speed, runCallbacks){};
+
+/**
+ * @public
+ * @param {boolean} deleteInstance
+ * @param {boolean} cleanupStyles
+ */
+Swiper.prototype.destroy = function(deleteInstance, cleanupStyles){};
+
+/**
+ * @public
+ * @type {Object}
+ */
+Swiper.prototype.autoplay;
+
+/**
+ * @public
+ */
+Swiper.prototype.autoplay.start = function(){};
+
+/**
+ * @public
+ */
+Swiper.prototype.autoplay.stop = function(){};
+
+/**
+ * @public
+ */
+Swiper.prototype.update = function(){};
+
+/**
+ * @public
+ */
+Swiper.prototype.updateSize = function(){}
+
+/**
+ * @public
+ */
+Swiper.prototype.updateSlides = function(){}
+
+/**
+ * @public
+ */
+Swiper.prototype.updateProgress = function(){}
+
+/**
+ * @public
+ */
+Swiper.prototype.updateSlidesClasses = function(){}
+
+/**
+ * @public
+ * @param {Element|string|Array<Element>} slide
+ */
+Swiper.prototype.prependSlide = function(slide){};
+
+/**
+ * @public
+ * @param {Element|string|Array<Element>} slide
+ */
+Swiper.prototype.appendSlide = function(slide){};
+
+/**
+ * @public
+ * @param {Array<number>|number} slideIndex
+ */
+Swiper.prototype.removeSlide = function(slideIndex){};
---
+++ externs/packery.js
@@ -0,0 +1,21 @@
+/**
+ * @externs
+ */
+
+/**
+ * @constructor
+ * @param {Element} wrapper
+ * @param {Object} config
+ */
+var Packery = function(wrapper, config){};
+
+/**
+ * @public
+ */
+Packery.prototype.layout = function(){};
+
+/**
+ * @public
+ * @param {Array<Element>|Element} elems
+ */
+Packery.prototype.addItems = function(elems){};
